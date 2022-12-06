d305a34ad147f76357fe63ec5aa9fe426699993d
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-12-06 11:32:43 +0100

    WIP: initial work on Ansible Script

---
+++ templates/start-peertube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source $HOME/.profile
+export NODE_ENV=production
+export NODE_CONFIG_DIR=/home/pacs/{{pac}}/users/{{user}}/config
+cd /home/pacs/{{pac}}/users/{{user}}/peertube-latest
+node dist/server &
