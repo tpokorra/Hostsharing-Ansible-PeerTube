918d4cd58f87eef02d1bb3feeb37e4d1dad41793
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-12-07 10:46:52 +0100

    make the website run

--- templates/start-peertube.sh
+++ templates/start-peertube.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source $HOME/.profile
+export PID=$HOME/var/run/peertube.pid
 export NODE_ENV=production
 export NODE_CONFIG_DIR=/home/pacs/{{pac}}/users/{{user}}/config
 cd /home/pacs/{{pac}}/users/{{user}}/peertube-latest
