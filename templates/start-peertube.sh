66cc03c339bee1972f89de33eeda5359d8712cbb
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-12-09 06:27:01 +0100

    fixes for the start script

--- templates/start-peertube.sh
+++ templates/start-peertube.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export PATH=$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+export HOME=/home/pacs/{{pac}}/users/{{user}}
 source $HOME/.profile
 export PID=$HOME/var/run/peertube.pid
 export NODE_ENV=production
-export NODE_CONFIG_DIR=/home/pacs/{{pac}}/users/{{user}}/config
-cd /home/pacs/{{pac}}/users/{{user}}/peertube-latest
-node dist/server &
+export NODE_CONFIG_DIR=$HOME/config
+cd $HOME/peertube-latest
+node dist/server >$HOME/var/log/gitea.log 2>&1 &
 echo $! > $PID
