929e6f9112465b9a10a67ce58ecaf30baf230d7f
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-12-09 06:38:04 +0100

    fixes for resetpwd.sh

--- templates/resetpwd.sh
+++ templates/resetpwd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 source $HOME/.profile
-export PT_INITIAL_ROOT_PASSWORD="{{adminpassword}}"
+
+# this only works on the first start of the peertube service
+#export PT_INITIAL_ROOT_PASSWORD="{{adminpassword}}"
+
 export NODE_CONFIG_DIR=/home/pacs/{{pac}}/users/{{user}}/config
 export NODE_ENV=production
+cd $HOME/peertube-latest
 npm run reset-password -- -u root || exit -1
 
